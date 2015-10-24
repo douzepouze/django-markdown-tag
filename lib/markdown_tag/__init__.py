3f6ea7f346d1119d90b5aa330887268a34f86539
Steffen Goertz <steffen@steffen-goertz.de>
2015-10-25 00:54:57 +0200

    renamed stuff.

--- lib/markdown_tag/__init__.py
+++ lib/markdown_tag/__init__.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
 # Copyright (c) 2008-2010 ActiveState Corp.
+# Copyright (c) 2015 Steffen Görtz
 # License: MIT (http://www.opensource.org/licenses/mit-license.php)
 
 r"""A small Django app that provides template tags for Markdown using the
-python-markdown2 library.
+python-markdown library.
 
-See <http://github.com/trentm/django-markdown-deux> for more info.
+See <http://github.com/douzepouze/django-markdown-tag> for more info.
 """
 
-__version_info__ = (1, 0, 6)
+__version_info__ = (1, 0, 0)
 __version__ = '.'.join(map(str, __version_info__))
-__author__ = "Trent Mick"
+__author__ = "Steffen Görtz"
 
 
 def markdown(text, style="default"):
     if not text:
         return ""
-    import markdown2
-    return markdown2.markdown(text, **get_style(style))
+    import markdown
+    return markdown.markdown()
 
 def get_style(style):
-    from markdown_deux.conf import settings
+    from markdown_tag.conf import settings
     try:
         return settings.MARKDOWN_DEUX_STYLES[style]
     except KeyError:
